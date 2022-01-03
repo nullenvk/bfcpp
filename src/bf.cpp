cc8d345e6a791014c68553c73e0f59dd36cb1d5a
nullenvk <nullenvk@protonmail.com>
2022-01-03 18:18:43 +0100

    Working

--- src/bf.cpp
+++ src/bf.cpp
@@ -1,4 +1,5 @@
 #include "bf.h"
+#include <cstdio>
     
 VM::VM(const std::vector<InstrType> code, size_t memSize) : code(code) {
     this->mem = new uint8_t[memSize];
@@ -8,7 +9,94 @@ VM::~VM() {
     delete this->mem;
 }
 
+void VM::mpMove(int size) {
+    mp += size;
+
+    if(mp < 0)
+        mp = memSize + mp;
+    else if(mp >= memSize)
+        mp = mp % memSize;
+}
+
+void VM::loopL() {
+    if(mem[mp] == 0) {
+        int depth = 1;
+
+        cp++;
+        while(depth != 0) {
+            if(code[cp] == INSTR_LOOPL) depth++;
+            else if(code[cp] == INSTR_LOOPR) depth--;
+            cp++;
+
+            if(cp >= code.size() || cp < 0)
+                return;
+        }
+
+    } else {
+        cp++;
+    }
+}
+
+void VM::loopR() {
+    if(mem[mp] == 0) {
+        cp++;
+        return;
+    }
+
+    int depth = 1;
+    while(depth != 0) {
+        cp--;
+        if(code[cp] == INSTR_LOOPL) depth--;
+        else if(code[cp] == INSTR_LOOPR) depth++;
+            
+        if(cp >= code.size() || cp < 0)
+            return;
+    }
+}
+
 void VM::run() {
     while(1) {
+        switch(code[cp]) {
+            case INSTR_LEFT:
+                mpMove(-1);
+                cp++;
+                break;
+
+            case INSTR_RIGHT:
+                mpMove(1);
+                cp++;
+                break;
+
+            case INSTR_PLUS:
+                mem[mp]++;
+                cp++;
+                break;
+            
+            case INSTR_MINUS:
+                mem[mp]--;
+                cp++;
+                break;
+
+            case INSTR_WRITE:
+                putchar(mem[mp]);
+                cp++;
+                break;
+
+            case INSTR_READ:
+                mem[mp] = getchar();
+                cp++;
+                break;
+
+            case INSTR_LOOPL:
+                loopL();
+                break;
+            
+            case INSTR_LOOPR:
+                loopR();
+                break;
+        }
+
+        if(cp >= code.size() || cp < 0)
+            break;
     }
 }
--- src/bf.h
+++ src/bf.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <cstdint>
+#include <stack>
 
 enum InstrType {
     INSTR_LEFT,
@@ -21,6 +22,8 @@ class VM {
     size_t cp = 0, mp = 0; // Code pointer, Memory pointer
 
     void mpMove(int side); // -1 - left, 1 - right
+    void loopL();
+    void loopR();
 public:
     VM(const std::vector<InstrType> code, size_t memSize);
     ~VM();

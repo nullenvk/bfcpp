419d4dfc7d80980797fec9ee587b1f1bcc951700
lxeiqr <lxeiqr@protonmail.com>
2022-01-19 18:48:58 +0100

    ASM Gen x64: Loops

---
+++ out.s
@@ -0,0 +1,10 @@
+SECTION .text
+global bfmain
+bfmain:
+loop_1:
+loop_1_2:
+jnz loop_1_2
+loop_1_11:
+jnz loop_1_11
+jnz loop_1
+ret
--- src/asmgen_x64.cpp
+++ src/asmgen_x64.cpp
@@ -1,7 +1,11 @@
 #include "asmgen_x64.h"
 
+#include <sstream>
+
 // This generator assumes NASM syntax
 // bfmain accepts cell address in rax
+
+typedef std::deque< std::pair<const IR::Tree*, size_t> > RecursionStack; // size_t - position
 
 void genPrologue(std::ostream& out) {
     out << "SECTION .text" << std::endl
@@ -10,31 +14,58 @@ void genPrologue(std::ostream& out) {
 }
 
 void genEpilogue(std::ostream& out) {
-    out << "    ret" << std::endl;
+    out << "ret" << std::endl;
+}
+
+std::string recursionToLoopName(RecursionStack rec) {
+    std::stringstream ns;
+
+    ns << "loop";
+
+    while(!rec.empty()) {
+        ns << "_" << std::to_string(rec.front().second);
+        rec.pop_front();
+    }
+
+    return ns.str();
+}
+
+void genStartLoop(std::ostream& out, RecursionStack rec) {
+    out << recursionToLoopName(rec) << ":" << std::endl;
+}
+
+void genEndLoop(std::ostream& out, RecursionStack rec) {
+    out << "jnz " << recursionToLoopName(rec) << std::endl;
 }
 
 void AsmGen_x64::gen(std::ostream& out, const IR::Tree ir) {
     genPrologue(out);
 
     // Tree recursion
-    std::stack< std::pair<const IR::Tree*, size_t> > recursion;
-    recursion.push(std::pair(&ir, 0));
+    RecursionStack recursion;
+    recursion.push_back(std::pair(&ir, 0));
     
     while(!recursion.empty()) {
-        const IR::Tree *node = recursion.top().first;
-        size_t& pos = recursion.top().second;
+        const IR::Tree *node = recursion.back().first;
+        size_t& pos = recursion.back().second;
 
         if(pos >= node->atoms.size()) {
-            recursion.pop();
-            if(!recursion.empty())
-                recursion.top().second++;
+            recursion.pop_back();
+
+            if(!recursion.empty()) {
+                genEndLoop(out, recursion);
+                recursion.back().second++;
+            }
+
             continue;
         }
 
         if(node->atoms[pos].type == IR::Atom::INSTR)
             pos++;
-        else
-            recursion.push(std::pair(&node->atoms[pos].loop, 0));
+        else {
+            genStartLoop(out, recursion);
+            recursion.push_back(std::pair(&node->atoms[pos].loop, 0));
+        }
     }
 
     genEpilogue(out);
